6ee89fa48d7791209de29fadef2679dfc0940339
Numan Naeem <numan.naeem9@gmail.com>
2022-10-19 22:44:29 +0530

    amount bg

--- src/components/ExpenseCard.js
+++ src/components/ExpenseCard.js
@@ -5,8 +5,8 @@ function ExpenseCard({amount, name, date}) {
 	return (
 		<div className='expense-card'>
 			<div className='card-body'>
-				<h3>{name} - <span>₹{amount}</span></h3>
-				<h6>{date.toLocaleString('en-gb',  { hour12: true }).toUpperCase()}</h6>
+				<h3>{name} <span>₹{amount}</span></h3>
+				<h6>{date.toLocaleDateString('en-gb',  { hour12: true, }).toUpperCase()}</h6>
 			</div>
 			<button><AiOutlineDelete /></button>
 		</div>
