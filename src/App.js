6af249fab34dcb4b2c569b273c33e2ded6c2f412
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-10-19 22:43:51 +0530

    fix : style

--- src/App.js
+++ src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
 
     <div className="App">
-      <ExpenseAdder />
-      <div style={{display: 'flex', flexDirection:'column', padding: '16px'}}>
-        {/* <ExpenseCard name={"Food"} amount={200} date={new Date(Date.now())} /> */}
+      <div className='wrapper'>
+        <ExpenseAdder />
+        <ExpenseCard name={"Food"} amount={200} date={new Date(Date.now())} />
       </div>
     </div>
   );
