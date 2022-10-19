81ff3da4305b3acfec319d64ee4d1912f2df6646
Numan Naeem <numan.naeem9@gmail.com>
2022-10-20 00:25:50 +0530

    Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,8 +4,9 @@ function Header({ amount }) {
     return (
         <div className='header'>
             <h1>Expense Tracker</h1>
-            <div>
-                <h4>total expense : <span>{amount}₹</span></h4>
+            <div className='header-total' >
+                <span>Total Spent</span>
+                <span>₹ {amount}</span>
             </div>
         </div>
     )
