18e6c87ad32defbfd73b1275e9c3af58a4480efc
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-10-19 23:16:07 +0530

    spend

--- src/components/ExpenseAdder.js
+++ src/components/ExpenseAdder.js
@@ -23,12 +23,12 @@ function ExpenseAdder(props) {
             <form onSubmit={handleSubmit} className='expense-adder'>
                 <div>
                     <label>Name </label>
-                    <input onChange={(e) => { console.log(e.target.value); setName(e.target.value) }} value={name} type="text" placeholder="Where you have Spent?" />
+                    <input onChange={(e) => { console.log(e.target.value); setName(e.target.value) }} value={name} type="text" placeholder="Where did you spend?" />
                 </div>
                 <div>
                     <label>Cost </label>
                     <input onChange={(e) => { setAmount(e.target.value) }} value={amount} className='amount' type="number" />
-                    <span> INR</span>
+                    <span>₹</span>
                 </div>
                 <div>
                     <label>Date </label>
