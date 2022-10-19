c6dc288126b59cae579d2b1d9da5dcd7fd4eceb6
swasthikshetty10 <swasthikshetty10902@gmail.com>
2022-10-19 21:56:22 +0530

    +dir components folder

---
+++ src/components/ExpenseAdder.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function ExpenseAdder() {
+
+    return (
+        <div>
+            <form >
+                <div>
+                    <label htmlFor=""></label>
+                </div>
+                <div>
+
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default ExpenseAdder
